3d6949415975ee3efead2fe217377cab9a43cc31
Bo Xu <bo_xu@foxitsoftware.com>
2014-06-30 21:41:41 -0700

    merge from fxcore change; adjust gyp file

---
+++ core/src/fxge/skia/fx_skia.h
@@ -0,0 +1,15 @@
+// Copyright 2014 PDFium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Original code copyright 2014 Foxit Software Inc. http://www.foxitsoftware.com
+
+#ifndef _FX_SKIA_H_
+#define _FX_SKIA_H_
+#include "SkBlitter.h"
+#include "SkDashPathEffect.h"
+#include "SkTLazy.h"
+#include "SkScan.h"
+#include "SkRasterClip.h"
+#include "SkStroke.h"
+#endif
