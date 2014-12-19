175183724a0ae115d43284e3b629098591d8315d
Bo Xu <bo_xu@foxitsoftware.com>
2014-12-19 00:36:56 -0800

    Add missing commit

---
+++ core/src/fxge/freetype/fx_freetype.h
@@ -0,0 +1,15 @@
+// Copyright 2014 PDFium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+ 
+// Original code copyright 2014 Foxit Software Inc. http://www.foxitsoftware.com
+
+#ifndef _FX_SRC_FREETYPE_H_
+#define _FX_SRC_FREETYPE_H_
+#include "../../../third_party/freetype/include/ft2build.h"
+#include "../../../third_party/freetype/include/freetype.h"
+#include "../../../third_party/freetype/include/ftoutln.h"
+#include "../../../third_party/freetype/include/ftmm.h"
+#include "../../../third_party/freetype/include/internal/ftobjs.h"
+#include "../../../third_party/freetype/include/tttables.h"
+#endif
