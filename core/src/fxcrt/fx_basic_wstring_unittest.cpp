3320483a06dfc0a84dafb4548201e5ec391101d7
Bo Xu <bo_xu@foxitsoftware.com>
2015-01-20 17:51:53 -0800

    Add test check

--- core/src/fxcrt/fx_basic_wstring_unittest.cpp
+++ core/src/fxcrt/fx_basic_wstring_unittest.cpp
@@ -7,13 +7,23 @@
 #include "../../include/fxcrt/fx_basic.h"
 
 TEST(fxcrt, WideStringUTF16LE_Encode) {
-  CFX_WideString wide_strings[] = {L"", L"abc", L"abcdef", L"abc\0def",
-	  L"\x4F60\x597d"};
+  CFX_WideString wide_strings[] = {
+	  L"",
+	  L"abc",
+	  L"abcdef",
+	  L"abc\0def",
+	  L"123\0456",
+	  L"\x3132\x6162"
+  };
   CFX_ByteString byte_strings[] = {
 	  CFX_ByteString(FX_BSTRC("\0\0")),
-	  CFX_ByteString(FX_BSTRC("a\0b\0c\0")),
-	  CFX_ByteString(FX_BSTRC("a\0b\0c\0d\0e\0f\0")),
-	  CFX_ByteString(FX_BSTRC("a\0b\0c\0\0\0d\0e\0f\0")),
-	  CFX_ByteString(FX_BSTRC("\x4F60\x597d")),
+	  CFX_ByteString(FX_BSTRC("a\0b\0c\0\0\0")),
+	  CFX_ByteString(FX_BSTRC("a\0b\0c\0d\0e\0f\0\0\0")),
+	  CFX_ByteString(FX_BSTRC("a\0b\0c\0\0\0")),
+	  CFX_ByteString(FX_BSTRC("1\02\03\0%\06\0\0\0")),
+	  CFX_ByteString(FX_BSTRC("12ab\0\0"))
+  };
+  for (size_t i = 0; i < FX_ArraySize(wide_strings); ++i) {
+	  EXPECT_EQ(byte_strings[i], wide_strings[i].UTF16LE_Encode()) << " for case number " << i;
   }
 }
